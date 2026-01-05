18b034a99ab796ff72cdc72b37e15577d591492a
Raghuveer Ch S <raghuveer57@users.noreply.github.com>
2026-01-05 06:54:29 +0530

    Refactor main function and add poem text file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String>               =   env::args().collect();
     
-    let query = &args[1];
-    let filename = &args[2];   
+    let (query, filename)   =   parse_config(&args);  
 
-    println!("Searching for '{}' in file '{}'", query, filename);
+    println!    ("Searching for '{}' in file '{}'", query, filename);
+
+    let contents =   fs::read_to_string(filename)
+                                    .expect("Something went wrong reading the file");
+
+    print!      ("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    
+    let query       =   &args[1];
+    let filename    =   &args[2];
+
+    (query, filename)
 }
