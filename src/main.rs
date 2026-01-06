a87f0939cb565b6a10d298648f8eed66a5566bae
Raghuveer Ch S <raghuveer57@users.noreply.github.com>
2026-01-06 05:50:16 +0530

    Refactor argument parsing by consolidating into Config struct

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,33 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args: Vec<String>               =   env::args().collect();
+    let args: Vec<String>       =   env::args().collect();
     
-    let (query, filename)   =   parse_config(&args);  
+    let config          =   Config::new(&args);  
 
-    println!    ("Searching for '{}' in file '{}'", query, filename);
+    println!    ("Searching for '{}' in file '{}'", config.query, config.filename);
 
-    let contents =   fs::read_to_string(filename)
+    let contents        =   fs::read_to_string(config.filename)
                                     .expect("Something went wrong reading the file");
 
     print!      ("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    
-    let query       =   &args[1];
-    let filename    =   &args[2];
+struct Config {
+    query:      String,
+    filename:   String,
+}
 
-    (query, filename)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+
+        let query       =   args[1].clone();
+        let filename    =   args[2].clone();
+
+        Config { query, filename }
+    }
+    
 }
