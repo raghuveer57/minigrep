b15e44ebb308a17e6ca9bec5c1dba29ee950e191
Raghuveer Ch S <raghuveer57@users.noreply.github.com>
2026-01-06 06:12:15 +0530

    Refactor argument handling in main function to improve error handling

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,27 @@ use std::fs;
 fn main() {
     let args: Vec<String>       =   env::args().collect();
     
-    let config          =   Config::new(&args);  
+    let config          =   Config::build(&args).unwrap_or_else(|err|{
+        print!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+                                      
+    run(config).unwrap_or_else(|err|{
+        print!("Application error: {}", err);
+        std::process::exit(1);
+    });
+    
+}
 
+fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     println!    ("Searching for '{}' in file '{}'", config.query, config.filename);
+    
+    let contents        =   fs::read_to_string(config.filename)?;
+                                    
 
-    let contents        =   fs::read_to_string(config.filename)
-                                    .expect("Something went wrong reading the file");
+    print!      ("With text:\n{}", contents); 
 
-    print!      ("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
@@ -20,15 +33,27 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    
+    // fn new(args: &[String]) -> Config {
+    //     if args.len() < 3 {
+    //         panic!("Not enough arguments");
+    //     }
+
+    //     let query       =   args[1].clone();
+    //     let filename    =   args[2].clone();
+
+    //     Config { query, filename }
+    // }
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
 
         let query       =   args[1].clone();
         let filename    =   args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
     
 }
